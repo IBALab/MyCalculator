7419e26619881b253906197fb193f72a8fd537a3
Ilya Fyodorov <fyodik95@gmail.com>
2016-04-20 22:05:49 +0300

    added MyDecompressor.java class

---
+++ MyCalculator/src/main/java/com/lab/calc/MyDecompressor.java
@@ -0,0 +1,93 @@
+package com.lab.calc;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+public class MyDecompressor {
+
+	public List<File> unTar(final File inputFile, final File outputDir)
+			throws FileNotFoundException, IOException, ArchiveException {
+
+		final List<File> untaredFiles = new LinkedList<File>();
+		final InputStream is = new FileInputStream(inputFile);
+		final TarArchiveInputStream debInputStream = (TarArchiveInputStream) new ArchiveStreamFactory()
+				.createArchiveInputStream("tar", is);
+		TarArchiveEntry entry = null;
+		while ((entry = (TarArchiveEntry) debInputStream.getNextEntry()) != null) {
+			final File outputFile = new File(outputDir, entry.getName());
+			if (entry.isDirectory()) {
+				if (!outputFile.exists()) {
+					if (!outputFile.mkdirs()) {
+						throw new IllegalStateException(
+								String.format("Couldn't create directory %s.", outputFile.getAbsolutePath()));
+					}
+				}
+			} else {
+				final OutputStream outputFileStream = new FileOutputStream(outputFile);
+				IOUtils.copy(debInputStream, outputFileStream);
+				outputFileStream.close();
+			}
+			untaredFiles.add(outputFile);
+		}
+		debInputStream.close();
+
+		return untaredFiles;
+	}
+
+	public File unGzip(final File inputFile, final File outputDir) throws FileNotFoundException, IOException {
+
+		final File outputFile = new File(outputDir, inputFile.getName().substring(0, inputFile.getName().length() - 3));
+
+		final GZIPInputStream in = new GZIPInputStream(new FileInputStream(inputFile));
+		final FileOutputStream out = new FileOutputStream(outputFile);
+
+		IOUtils.copy(in, out);
+
+		in.close();
+		out.close();
+
+		return outputFile;
+	}
+
+	public void decompressor(String sourceDirPath, String targetDirPath)
+			throws FileNotFoundException, IOException, ArchiveException {
+		File sourceDir = new File(sourceDirPath);
+		File targetDir = new File(targetDirPath);
+		File[] listOfFiles = sourceDir.listFiles();
+		Arrays.sort(listOfFiles);
+		MyDecompressor datz = new MyDecompressor();
+		for (int i = 0; i < listOfFiles.length; i++) {
+			File currentInputFile = listOfFiles[i];
+			if (currentInputFile.isFile() && currentInputFile.getName().endsWith(".tar.gz")) {
+				datz.unTar(datz.unGzip(currentInputFile, targetDir), targetDir);
+			}
+		}
+		File[] listOfFilesTargetFolder = targetDir.listFiles();
+		Arrays.sort(listOfFilesTargetFolder);
+		for (int i = 0; i < listOfFilesTargetFolder.length; i++) {
+			File targetTarFile = listOfFilesTargetFolder[i];
+			try {
+				if (targetTarFile.isFile() && targetTarFile.getName().endsWith(".tar")) {
+					targetTarFile.delete();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
