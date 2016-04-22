e49768cd0d33cbd76cadf5546f6ae1b97ff9dd2d
pitskod <pitsko.d@gmail.com>
2016-04-22 12:10:23 +0300

    added MyCalculatorMain.class, TestMyCalculatorMain.class and Resources for TestMyCalculatorMain

--- MyCalculator/src/main/java/com/lab/calc/MyCalculatorMain.java
+++ MyCalculator/src/main/java/com/lab/calc/MyCalculatorMain.java
@@ -1,78 +1,108 @@
 package com.lab.calc;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
+import org.apache.commons.compress.archivers.ArchiveException;
+
 public class MyCalculatorMain {
 
-	public static void main(String[] args) {
-				
-		if (args.length == 3){
+	public static void main(String[] args) throws Exception {
+
+		if (args.length == 3) {
 			String pathToInputFolder = args[0].trim();
 			String pathToOutputFolder = args[1].trim();
 			String mathOperation = args[2].trim().toLowerCase();
-			
-			MyDecompression(pathToInputFolder);
-			ArrayList<Integer> allValuesList = null;
-			try {
-				allValuesList = MySourceReading(pathToInputFolder+"/decompressed");
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+
+			String pathToOutputTarGz = pathToInputFolder + "/" + "decompressed";
+			new File(pathToOutputTarGz).mkdir();
+			MyDecompression(pathToInputFolder, pathToOutputTarGz);
+			ArrayList<Integer> allValuesList = MySourceReading(pathToOutputTarGz);
 			Integer result = null;
 			try {
-				result = MyMathDoing(allValuesList,mathOperation);
+				result = MyMathDoing(allValuesList, mathOperation);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			try {
-				MyResultWriter(result,pathToOutputFolder);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		else System.out.println("Wrong number of parameters!");
+
+			MyResultWriter(result, pathToOutputFolder);
+		} else
+			throw new Exception("Wrong number of parameters!");
 
 	}
-	
-	
-	public static void MyDecompression(String pathToInputFolder){
-		//MyDecompressor md = new MyDecompressor();
-		//md.decompressAllTarGz(pathToInputFolder);
+
+
+	public static void MyDecompression(String pathToInputFolder, String pathToOutputFolder) {
+		MyDecompressor md = new MyDecompressor();
+		try {
+			md.decompressor(pathToInputFolder, pathToOutputFolder);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ArchiveException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	
-	public static ArrayList<Integer> MySourceReading(String pathToInputFolder) throws FileNotFoundException{
+
+
+	public static ArrayList<Integer> MySourceReading(String pathToInputFolder) {
 		MySourceReader msr = new MySourceReader();
-		return msr.getAllValues(pathToInputFolder);
+		try {
+			return msr.getAllValues(pathToInputFolder);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
-	
-	public static Integer MyMathDoing(ArrayList<Integer> allValuesList,String mathOperation) throws Exception {
+
+
+	public static Integer MyMathDoing(ArrayList<Integer> allValuesList, String mathOperation) throws Exception {
 		MyMath mm = new MyMath();
 		Integer resultValue = null;
-		switch (mathOperation){
-			case "sum" : resultValue = mm.sum(allValuesList) ; break;
-			case "min" : resultValue = mm.min(allValuesList) ; break;
-			case "max" : resultValue = mm.max(allValuesList) ; break;
-			case "evenminusodd" : resultValue = mm.EvenMinusOdd(allValuesList) ; break;
-			case "mean" : resultValue = mm.Mean(allValuesList) ; break;
-			default : throw new  Exception ("Invalid math operation") ;
+		switch (mathOperation) {
+		case "sum":
+			resultValue = mm.sum(allValuesList);
+			break;
+		case "min":
+			resultValue = mm.min(allValuesList);
+			break;
+		case "max":
+			resultValue = mm.max(allValuesList);
+			break;
+		case "evenminusodd":
+			resultValue = mm.EvenMinusOdd(allValuesList);
+			break;
+		case "mean":
+			resultValue = mm.Mean(allValuesList);
+			break;
+		default:
+			throw new Exception("Invalid math operation");
 		}
 		return resultValue;
 	}
-	
-	
-	private static void MyResultWriter(Integer result, String pathToOutputFolder) throws IOException {
-		FileWriter fileWriter = new FileWriter(pathToOutputFolder);
-		PrintWriter printWriter = new PrintWriter(fileWriter);
-		printWriter.println(result);
-		printWriter.close();
+
+
+	private static void MyResultWriter(Integer result, String pathToOutputFolder) {
+		FileWriter fileWriter;
+		try {
+			fileWriter = new FileWriter(pathToOutputFolder);
+			PrintWriter printWriter = new PrintWriter(fileWriter);
+			printWriter.println(result);
+			printWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
 
 }
--- MyCalculator/src/test/java/com/lab/calc/test/TestMyCalculatorMain.java
+++ MyCalculator/src/test/java/com/lab/calc/test/TestMyCalculatorMain.java
@@ -7,33 +7,103 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.lab.calc.MyCalculatorMain;
 
 public class TestMyCalculatorMain {
+	static String pathToInputFolder = "src/test/resources";
+	static String pathToOutputFolder = pathToInputFolder + "/MyCalculatorTestResults";
 
-	@Before
-	public void setUp() throws Exception {
+
+	@BeforeClass
+	public static void setUp() throws Exception {
+		new File(pathToOutputFolder).mkdir();
 	}
+
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
+
 	@Test
-	public void main() throws FileNotFoundException {
-		String args [] = new String []{"/home/admin/docs/source_archive","/home/admin/docs/source_archive/input.txt","MIN"};
+	public void mainForMin() throws Exception {
+		// min
+		String args[] = new String[] { pathToInputFolder, pathToOutputFolder + "/minResult.txt", "MIN" };
 		MyCalculatorMain.main(args);
-		Integer rightResult = 32, calcResult = null;
-		File outputFile = new File (args[1]);
+		Integer rightMin = 6;
+		File outputFile = new File(args[1]);
+		Integer calcResult = readFromFile(outputFile);
+		assertEquals(rightMin, calcResult);
+	}
+
+
+	@Test
+	public void mainForMax() throws Exception {
+		// max
+		String args[] = new String[] { pathToInputFolder, pathToOutputFolder + "/maxResult.txt", "Max" };
+		MyCalculatorMain.main(args);
+		Integer rightMax = 30522;
+		File outputFile = new File(args[1]);
+		Integer calcResult = readFromFile(outputFile);
+		assertEquals(rightMax, calcResult);
+
+	}
+
+
+	@Test
+	public void mainForSum() throws Exception {
+		// sum
+		String args[] = new String[] { pathToInputFolder, pathToOutputFolder + "/sumResult.txt", "Sum" };
+		MyCalculatorMain.main(args);
+		Integer rightSum = 59555;
+		File outputFile = new File(args[1]);
+		Integer calcResult = readFromFile(outputFile);
+		assertEquals(rightSum, calcResult);
+	}
+
+
+	@Test
+	public void mainForEvenMinusOdd() throws Exception {
+		// evenminusodd
+		String args[] = new String[] { pathToInputFolder, pathToOutputFolder + "/evenMinusOddResult.txt",
+				"evenminusodd" };
+		MyCalculatorMain.main(args);
+		Integer rightResult = 30909;
+		File outputFile = new File(args[1]);
+		Integer calcResult = readFromFile(outputFile);
+		assertEquals(rightResult, calcResult);
+
+	}
+
+
+	@Test
+	public void mainForMean() throws Exception {
+		// mean
+		String argsForMax[] = new String[] { pathToInputFolder, pathToOutputFolder + "/meanResult.txt", "Mean" };
+		MyCalculatorMain.main(argsForMax);
+		Integer rightMean = 976;
+		File outputFile = new File(argsForMax[1]);
+		Integer calcResult = readFromFile(outputFile);
+		assertEquals(rightMean, calcResult);
+
+	}
+
+
+	@SuppressWarnings("resource")
+	private Integer readFromFile(File outputFile) throws Exception, FileNotFoundException {
 		Scanner scanner = new Scanner(outputFile);
-		while(scanner.hasNextInt())
+		Integer calcResult = null;
+		if (scanner.hasNextInt())
 			calcResult = scanner.nextInt();
+		else
+			throw new Exception("Wrong data in output file!");
+		if (scanner.hasNext())
+			throw new Exception("Wrong data in output file!");
 		scanner.close();
-		System.out.println(calcResult);
-		assertEquals(rightResult,calcResult);
+		return calcResult;
 	}
 
 }
